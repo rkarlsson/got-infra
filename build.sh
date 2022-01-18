3dcebfdcd98e4792b4827f4c8124c9e7c6efede4
Robert Karlsson <rkarlsson@gmail.com>
2022-01-18 00:48:02 +0000

    Added build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -d "./build" ]
+then
+	mkdir build
+fi
+cd build
+cmake -DBUILD_STATIC=ON -DBUILD_SHARED_LIBS=OFF -DENABLE_PUSH=OFF -DENABLE_COMPRESSION=OFF --parallel 4 ..
+make -j 5
